7b68c8fc32877da7492f925936f0d4995998bfc0
Anderson de Oliveira <anderson.java.dev@gmail.com>
2024-06-18 17:59:24 -0300

    ajuste rodape

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-content">
-                <p>© 2024 Psicólogas Online. Atendimento psicológico online focado no seu bem-estar emocional e mental.</p>
-                <p>Email: contato@psicologasonline.com | Telefone: (11) 1234-5678</p>
-                <p>Criado por <a href="https://www.aodev.com" target="_blank" rel="noopener noreferrer">Aodev</a></p>
+                <p>© 2024 Desatando Nós Psicologia. Atendimento psicológico online focado no seu bem-estar emocional e mental.</p>
+                <p>Email: contato@desatandonos.com.br | Telefone: (11) 1234-5678</p>
+                <p>Criado por <a href="https://www.aodev.com.br" target="_blank" rel="noopener noreferrer">Aodev</a></p>
             </div>
         </footer>
     );
